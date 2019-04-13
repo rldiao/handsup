fad3f00c9315c311ccbf88499786d236749395e3
Sii Kim Lau <s.lau22@student.unimelb.edu.au>
2019-04-13 13:12:28 +1000

    HomePage working.

---
+++ backend/models/db.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+//copy from CONNECT (MongoDB Atlas)
+const dbURI = process.env.DB || "NOPE";
+
+const options = {
+  useNewUrlParser: true,
+  dbName: "test"
+};
+
+mongoose.connect(dbURI, options).then(
+  () => {
+    console.log("Database connection established!");
+  },
+  err => {
+    console.log("Error connecting Database instance due to: ", err);
+  }
+);
+
+require("./donors");
+require("./receivers");
+require("./donations");
+
+//DB=mongodb+srv://lausk97:qwe123@cluster0-mkuyp.mongodb.net/test?retryWrites=true npm start
--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import HomePage from "./page/HomePage/HomePage";
 
 class App extends Component {
   state = {
@@ -8,32 +9,33 @@ class App extends Component {
   };
 
   componentDidMount() {
-      // Call our fetch function below once the component mounts
+    // Call our fetch function below once the component mounts
     this.callBackendAPI()
       .then(res => this.setState({ data: res.express }))
       .catch(err => console.log(err));
   }
-    // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
+  // Fetches our GET route from the Express server. (Note the route we are fetching matches the GET route from server.js
   callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
+    const response = await fetch("/express_backend");
     const body = await response.json();
 
     if (response.status !== 200) {
-      throw Error(body.message) 
+      throw Error(body.message);
     }
     return body;
   };
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        {/*  Render the newly fetched data inside of this.state.data  */}
-        <p className="App-intro">{this.state.data}</p>
-      </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <h1 className="App-title">Welcome to React</h1>
+      //   </header>
+      //   {/*  Render the newly fetched data inside of this.state.data  */}
+      //   <p className="App-intro">{this.state.data}</p>
+      // </div>
+      <HomePage />
     );
   }
 }
