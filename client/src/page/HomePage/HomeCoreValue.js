282719d471ecda2284a678a69b042f0af5c2b461
Sii Kim Lau <s.lau22@student.unimelb.edu.au>
2019-04-13 14:08:45 +1000

    homepage for mockup

--- client/src/page/HomePage/HomeCoreValue.js
+++ client/src/page/HomePage/HomeCoreValue.js
@@ -5,6 +5,8 @@ import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
 import Icon from "@material-ui/core/Icon";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
 
 const styles = {
   card: {
@@ -54,54 +56,62 @@ const styles = {
   rightSubHeading: {}
 };
 
-function ComplexGrid(props) {
+function FullWidthGrid(props) {
   const { classes } = props;
 
   return (
-    <Card className={classes.card}>
-      <CardMedia
-        className={classes.fstMedia}
-        image={require("../../assets/img/isaac-mehegan-747227-unsplash.jpg")}
-      />
+    <Grid container spacing={24}>
+      <Grid item xs={12} sm={6}>
+        <Paper className={classes.paper}>xs=12 sm=6</Paper>
+      </Grid>
+      <Grid item xs={12} sm={6}>
+        <Paper className={classes.paper}>xs=12 sm=6</Paper>
+      </Grid>
+    </Grid>
+    // <Card className={classes.card}>
+    //   <CardMedia
+    //     className={classes.fstMedia}
+    //     image={require("../../assets/img/isaac-mehegan-747227-unsplash.jpg")}
+    //   />
 
-      {/* // TODOS: div need to increase size */}
-      <div style={styles.overlay}>
-        <Typography className={classes.heading} style={{ fontSize: "2.5vw" }}>
-          Our Core Values
-          <br />
-          <Icon className={classes.leftIcon} color="primary">
-            accessibility_new
-          </Icon>
-          <Icon className={classes.rightIcon} color="primary">
-            accessibility
-          </Icon>
-          <br />
-          <Typography
-            className={[classes.heading, classes.leftSubHeading]}
-            style={{ fontSize: "1.25vw" }}
-          >
-            Stay open-minded
-          </Typography>
-          <Typography
-            className={[classes.heading, classes.rightContent]}
-            variant="subtitle1"
-          >
-            Stay open-minded
-          </Typography>
-        </Typography>
-      </div>
+    //   {/* // TODOS: div need to increase size */}
+    //   <div style={styles.overlay}>
+    //     <Typography className={classes.heading} style={{ fontSize: "2.5vw" }}>
+    //       Our Core Values
+    //     </Typography>
+    //     <br />
+    //     <Icon className={classes.leftIcon} color="primary">
+    //       accessibility_new
+    //     </Icon>
+    //     <Icon className={classes.rightIcon} color="primary">
+    //       accessibility
+    //     </Icon>
+    //     <br />
+    //     <Typography
+    //       className={[classes.heading, classes.leftSubHeading]}
+    //       style={{ fontSize: "1.25vw" }}
+    //     >
+    //       Stay open-minded
+    //     </Typography>
+    //     <Typography
+    //       className={[classes.heading, classes.rightContent]}
+    //       variant="subtitle1"
+    //     >
+    //       Stay open-minded
+    //     </Typography>
+    //   </div>
 
-      <CardMedia
-        className={classes.sndMedia}
-        image={require("../../assets/img/diego-ph-249471-unsplash.jpg")}
-        // style={{ margin: "5% 8% 5% 75%" }}
-      />
-    </Card>
+    //   <CardMedia
+    //     className={classes.sndMedia}
+    //     image={require("../../assets/img/diego-ph-249471-unsplash.jpg")}
+    //     // style={{ margin: "5% 8% 5% 75%" }}
+    //   />
+    // </Card>
   );
 }
 
-ComplexGrid.propTypes = {
+FullWidthGrid.propTypes = {
   classes: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(ComplexGrid);
+export default withStyles(styles)(FullWidthGrid);
--- client/src/page/HomePage/HomePage.js
+++ client/src/page/HomePage/HomePage.js
@@ -2,19 +2,10 @@ import React, { Component, Fragment } from "react";
 import HomeMainImage from "./HomeMainImage";
 import HomeCoreValue from "./HomeCoreValue";
 
-// class HomePage extends Component {
-//   render() {
-//     return (
-//       <HomeMainImage />
-//     );
-//   }
-// }
-
 const HomePage = () => {
   return (
     <Fragment>
       <HomeMainImage />
-      <HomeCoreValue />
     </Fragment>
   );
 };
