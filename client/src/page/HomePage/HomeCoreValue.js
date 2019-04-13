aca2261520f806dbd00aac7f15f3b9a5fd8736a4
Sii Kim Lau <s.lau22@student.unimelb.edu.au>
2019-04-13 14:34:26 +1000

    put dependency for material-ui

--- client/src/page/HomePage/HomeCoreValue.js
+++ client/src/page/HomePage/HomeCoreValue.js
@@ -1,10 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardMedia from "@material-ui/core/CardMedia";
-import Typography from "@material-ui/core/Typography";
-import Icon from "@material-ui/core/Icon";
+// import Card from "@material-ui/core/Card";
+// import CardMedia from "@material-ui/core/CardMedia";
+// import Typography from "@material-ui/core/Typography";
+// import Icon from "@material-ui/core/Icon";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 
