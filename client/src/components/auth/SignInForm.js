7cb2ca920eec402c30d2a65e82858530ee9997fa
Robert Diao <dia.90925@gmail.com>
2019-04-24 12:39:10 +1000

    before merge

--- client/src/components/auth/SignInForm.js
+++ client/src/components/auth/SignInForm.js
@@ -48,7 +48,7 @@ export default class SignInForm extends Component {
     render() {
         if(this.state.redirect === true) {
             return <Redirect to='/'/>
-        }
+    }
 
     return (
         <div className={styles.container}>
