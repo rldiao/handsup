e57da2efc0599275f2a714a3164487d5d5f6412f
yurongs <yurongs@unimelb.edu.au>
2019-05-13 10:19:59 +1000

    fix donee edit profile

--- client/src/components/doneeProfile/DoneeAbout.js
+++ client/src/components/doneeProfile/DoneeAbout.js
@@ -1,7 +1,8 @@
 import React, { Component, Fragment } from "react";
 import styles from "./doneeProfile.module.css";
-// import { profileStyles } from "../../pages/userProfile/profileStyles";
-// import { Button } from "@material-ui/core";
+import { userTypeConstants } from "../../constants";
+import { profileStyles } from "../../pages/userProfile/profileStyles";
+import { Button } from "@material-ui/core";
 
 export default class DoneeAbout extends Component {
   handleEditProfileClick = () => {
@@ -9,15 +10,22 @@ export default class DoneeAbout extends Component {
   };
 
   render() {
-    return (
-      <Fragment>
-        {/* <Button
+    const userType = this.props.donee.userType;
+    let editButton;
+    if (userType === userTypeConstants.DONEE) {
+      editButton = (
+        <Button
           variant="outlined"
           onClick={this.handleEditProfileClick}
           style={profileStyles.editProfileButton}
         >
           Edit Profile
-        </Button> */}
+        </Button>
+      );
+    }
+    return (
+      <Fragment>
+        {editButton}
         <h2 className={styles.aboutTitle}>Location</h2>
         <p className={styles.aboutText}>{this.props.donee.location}</p>
 
--- client/src/components/doneeProfile/MainInfo.js
+++ client/src/components/doneeProfile/MainInfo.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import styles from "./doneeProfile.module.css";
+import { userTypeConstants } from "../../constants";
 // import Axios from "axios";
 import { BorderLinearProgress } from "../userProfile/BorderLinearProgress";
 import { Button, Typography } from "@material-ui/core";
@@ -7,6 +8,20 @@ import { profileStyles } from "../../pages/userProfile/profileStyles";
 
 export default class MainInfo extends Component {
   render() {
+    const userType = this.props.donee.userType;
+    let donateButton;
+    if (userType === userTypeConstants.DONER) {
+      donateButton = (
+        <Button
+          variant="outlined"
+          // onClick={}
+          style={profileStyles.saveButton}
+        >
+          Donate Now
+        </Button>
+      );
+    }
+
     const progressWidth =
       (this.props.donee.funded / this.props.donee.monthlyDonationLimit) * 100;
 
@@ -30,13 +45,7 @@ export default class MainInfo extends Component {
             Until {this.props.donee.monthlyRenewalDate}
           </Typography>
           <br />
-          <Button
-            variant="outlined"
-            // onClick={}
-            style={profileStyles.saveButton}
-          >
-            Donate Now
-          </Button>
+          {donateButton}
         </div>
       </div>
     );
