1c2af3d91424479747b1de1ef1c1171202baecc7
Robert Diao <dia.90925@gmail.com>
2019-05-18 14:41:50 +1000

    added env dependency to auth middleware

--- backend/routes/donee.routes.js
+++ backend/routes/donee.routes.js
@@ -3,30 +3,20 @@ const doneeController = require("../controllers/donee.controller");
 const auth = require("../middleware/auth");
 const router = express.Router();
 
-const _auth = (req, res, next) => {
-  if (process.env.NODE_ENV === "development") {
-    next();
-  } else {
-    return auth.withAuth;
-  }
-};
-
 router.post("/signup", doneeController.createDonee);
 
 router.post("/login", doneeController.loginDonee);
 
 router.get("/logout", doneeController.logoutDonee);
 
-router.get("/", _auth, doneeController.getDonees);
+router.get("/", [auth.withAuth, doneeController.getDonees]);
 
-router.get("/:_id", doneeController.getOneDonee);
+router.get("/:_id", [auth.withAuth, doneeController.getOneDonee]);
 
-router.get("/:_id", [_auth], doneeController.getOneDonee);
+router.put("/new_post/:_id", [auth.withAuth, doneeController.addPostID]);
 
-router.put("/new_post/:_id", [_auth], doneeController.addPostID);
+router.put("/remove_post/:_id", [auth.withAuth, doneeController.removePostID]);
 
-router.put("/remove_post/:_id", [_auth], doneeController.removePostID);
-
-router.put("/update/:_id", [_auth], doneeController.updateOneDonee);
+router.put("/update/:_id", [auth.withAuth, doneeController.updateOneDonee]);
 
 module.exports = router;
