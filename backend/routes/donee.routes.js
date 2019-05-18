4985e15e805f5979022ef8d47052685facb1bc1b
Robert Diao <dia.90925@gmail.com>
2019-05-18 16:32:49 +1000

    merge conflict donee routes

--- backend/routes/donee.routes.js
+++ backend/routes/donee.routes.js
@@ -10,7 +10,6 @@ router.post("/login", doneeController.loginDonee);
 
 router.get("/logout", doneeController.logoutDonee);
 
-<<<<<<< HEAD
 router.get("/", [auth.withAuth, doneeController.getDonees]);
 
 router.get("/:_id", [auth.withAuth, doneeController.getOneDonee]);
@@ -20,18 +19,5 @@ router.put("/new_post/:_id", [auth.withAuth, doneeController.addPostID]);
 router.put("/remove_post/:_id", [auth.withAuth, doneeController.removePostID]);
 
 router.put("/update/:_id", [auth.withAuth, doneeController.updateOneDonee]);
-=======
-router.get("/", [auth.withAuth], doneeController.getDonees);
-
-router.get("/:_id", doneeController.getOneDonee);
-
-router.get("/:_id", [auth.withAuth], doneeController.getOneDonee);
-
-router.put("/new_post/:_id", [auth.withAuth], doneeController.addPostID);
-
-router.put("/remove_post/:_id", [auth.withAuth], doneeController.removePostID);
-
-router.put("/update/:_id", [auth.withAuth], doneeController.updateOneDonee);
->>>>>>> origin
 
 module.exports = router;
